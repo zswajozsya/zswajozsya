171ff53fb3f8e71a024c6b837db71b28ec0c74db
Mirus <lomirus.dev@protonmail.com>
2024-02-04 22:27:35 +0800

    Wrap return type of `get()` with Option

--- src/get/mod.rs
+++ src/get/mod.rs
@@ -2,10 +2,13 @@ use std::path::PathBuf;
 
 use crate::{error::Error, Directory};
 
-pub fn get<P: Into<PathBuf>>(path: P) -> Result<Directory, Error> {
+pub fn get<P: Into<PathBuf>>(path: P) -> Result<Option<Directory>, Error> {
     let mut path: PathBuf = path.into();
     path.push(".zswajozsya.ron");
+    if !path.exists() {
+        return Ok(None);
+    }
     let text = std::fs::read_to_string(path)?;
     let dir = ron::from_str(&text)?;
-    Ok(dir)
+    Ok(Some(dir))
 }
